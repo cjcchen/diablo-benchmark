fde07267261bc479607c902cbdb08521b88d2618
Ubuntu <ubuntu@ip-172-31-16-173.ec2.internal>
2023-04-04 18:15:03 +0000

    fix bug

--- blockchains/nresilientdb/resdb_client/client/client.go
+++ blockchains/nresilientdb/resdb_client/client/client.go
@@ -34,7 +34,7 @@ func MakeClient(ip string, port int) (c *Client, err error) {
 	c.pending_tasks = make(chan*resdb.Transaction, 1000000)
 	c.done_tasks = make(chan uint64, 1000000)
 
-  for i:=0; i < 100; i++ {
+  for i:=0; i < 50; i++ {
     go func(c * Client, ip string, port int){
          var client *resdb_client.TransactionClient
 
@@ -46,32 +46,34 @@ func MakeClient(ip string, port int) (c *Client, err error) {
           var tx_list []*resdb.Transaction
           var uids map[uint64]int32
           var ok bool
+          var j int
 
-
-          //tx_list = make([]*resdb.Transaction)
-
-          for j :=0; j < 50; j++ {
+          tx_list = make([]*resdb.Transaction, 50)
+          j = 0
+          innerloop : for j =0; j < 50; j++ {
             select {
               case tx,ok =<- c.pending_tasks:
                 if ok {
-                  tx_list = append(tx_list, tx)
+                  //tx_list = append(tx_list, tx)
+                  tx_list[j] = tx
                   continue
                 } else {
                   break loop
                 }
               case <-time.After(time.Microsecond):
-                break
+                break innerloop
               default:
-                break
+                break innerloop
             }
           }
 
+          tx_list = tx_list[0:j]
           if(len(tx_list) == 0) {
             //log.Printf("no data ")
             time.Sleep(time.Duration(time.Microsecond) * 100)
             continue
           }
-          //log.Printf("send uids:",len(tx_list))
+          //log.Printf("send uids: %d, j = %d\n",len(tx_list), j)
           uids,err = client.SendBatchTransaction(tx_list)
           if err != nil {
             return
