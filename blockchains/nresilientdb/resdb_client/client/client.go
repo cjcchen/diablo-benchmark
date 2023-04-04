475cd3325ab05d2e96299e19c4e1e8a9b6a18f6a
Ubuntu <ubuntu@ip-172-31-23-82.ec2.internal>
2023-04-04 06:30:00 +0000

    improve

--- blockchains/nresilientdb/resdb_client/client/client.go
+++ blockchains/nresilientdb/resdb_client/client/client.go
@@ -32,7 +32,7 @@ func MakeTransaction(uid uint64, from string, to string, amount uint64) *Transac
 type Client struct {
 	serverIp string
 	serverPort int
-	channel chan int
+	channel chan *resdb_client.TransactionClient
 	done_tasks chan *Transaction
   client *resdb_client.TransactionClient
 }
@@ -41,29 +41,42 @@ type Client struct {
 // MakeClient is the factory for constructing a Client for a given endpoint.
 func MakeClient(ip string, port int) (c *Client, err error) {
   c = &Client{}
-  c.client = resdb_client.MakeTransactionClient(ip,port)
-	c.channel =  make(chan int, 10)
-	c.done_tasks = make(chan *Transaction, 100)
+	c.channel =  make(chan *resdb_client.TransactionClient, 1000)
+	c.done_tasks = make(chan *Transaction, 100000)
+
+  for i:=0; i < 1000; i++ {
+    c.channel <- resdb_client.MakeTransactionClient(ip,port)
+  }
 	return
 }
 
 func (c *Client) SendTransaction(tx *Transaction) {
-  c.channel <- 1
+  var res_cli *resdb_client.TransactionClient
 
-  go func(c * Client, tx *Transaction){
+  res_cli =<- c.channel
+
+  go func(this *Client, c *resdb_client.TransactionClient, tx *Transaction){
       var uid uint64
       var err error
 
-      //log.Printf("tx done, uid %d", tx.uid)
-      uid,err = c.client.SendRawTransaction(tx.uid, tx.from, tx.to, tx.amount)
+      if tx.uid %10000 == 0 {
+        log.Printf("tx done, uid %d", tx.uid)
+      }
+      uid,err = c.SendRawTransaction(tx.uid, tx.from, tx.to, tx.amount)
       if (uid != tx.uid || err != nil) {
+        if err != nil {
+          log.Println("get error %s", err)
+        } else {
+          log.Println("get uid %d tx uid %d", uid, tx.uid)
+        }
         tx.committed = false
       } else {
+          //log.Println("get uid %d done", uid)
         tx.committed = true
       }
-      c.done_tasks <- tx
-      <-c.channel
-  }(c, tx)
+      this.done_tasks <- tx
+      this.channel <- c
+  }(c, res_cli, tx)
 
   //log.Printf("send txn done")
   return
@@ -74,7 +87,9 @@ func (c *Client) WaitNextTxn() (uid uint64, err error){
 	var ok bool
 	tx, ok = <-c.done_tasks
 	if ok {
-		//log.Printf("get one txn %d, status %d\n",tx.GetUid(), tx.committed)
+      if tx.uid %10000 == 0 {
+        log.Printf("get one txn %d, status %d\n",tx.GetUid(), tx.committed)
+      }
     if tx.committed {
       return tx.GetUid(), nil
     } else {
